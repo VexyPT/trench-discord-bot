9c07014154ed83ecc6a0da4e88d544795cd1f64d
Rincko <guilhermericardopro71@gmail.com>
2024-12-03 21:41:04 -0400

    emojis function update

--- src/functions/utils/emojis.ts
+++ src/functions/utils/emojis.ts
@@ -1,19 +1,29 @@
-import { settings } from "#settings";
 import { formatEmoji } from "discord.js";
+import fs from "node:fs/promises";
 
-type EmojiList = typeof settings.emojis;
-type EmojiKey = keyof EmojiList["animated"] | keyof EmojiList["static"];
-type IconInfo = { id: string, animated: boolean, toString(): string };
-type Icon = Record<EmojiKey, IconInfo>;
+type EmojiList = typeof import("#emojisfile");
+
+const filepath = process.env.ENV === "dev" ? "emojis.dev.json" : "emojis.json";
+const file = await fs.readFile(filepath, "utf-8");
+const emojis: EmojiList = JSON.parse(file);
+
+type IconKey = keyof EmojiList["animated"] | keyof EmojiList["static"];
+type IconInfo = { id: string, animated?: boolean, toString(): string };
+type Icon = Record<IconKey, IconInfo>;
 
 const icon: Icon = Object.create({});
 
-for(const [name, id] of Object.entries(settings.emojis.static)){
-    const data = { id, animated: false, toString: () => formatEmoji(id, false) };
-    Object.assign(icon, { [name]: data });
-}
-for(const [name, id] of Object.entries(settings.emojis.animated)){
-    const data = { id, animated: true, toString: () => formatEmoji(id, true) };
-    Object.assign(icon, { [name]: data });
-}
+const transform = (emojis: Record<string, string>, animated?: boolean) => {
+    for(const [name, id] of Object.entries(emojis)){
+        function toString(){
+            return formatEmoji(id, animated);
+        }
+        const data = { id, animated, toString };
+        Object.assign(icon, { [name]: data });
+    }
+};
+
+transform(emojis.static);
+transform(emojis.animated, true);
+
 export { icon };
